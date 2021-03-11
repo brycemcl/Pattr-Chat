be62d5b175fc2411ce092c88740b4fca7cba5d27
Bryce McLachlan <brycemclachlan@gmail.com>
2021-03-11 02:26:32 +0000

    added files for docker image

--- services/backend/service/__tests__/root.test.js
+++ services/backend/service/__tests__/root.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest')
 const app = require('../app')
-
+// https://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/
 describe('Test the root path', () => {
   test('It should response the GET method', async () => {
     const response = await request(app).get('/')
