e7f17415f33c87ff631346c9f1bcaad1f66c5162
Bryce McLachlan <brycemclachlan@gmail.com>
2021-03-12 02:53:21 +0000

    breakdown of the slack ui into a component tree

---
+++ design/main screen breakdown .js
@@ -0,0 +1,30 @@
+// function App () {
+//   return (
+//     <>
+//       <Header />
+//       <Sidebar>
+//         <SidebarHeader>
+//           <OrgSwitcher />
+//           <NewMessage />
+//         </SidebarHeader>
+//         <SidebarMain>
+//           <AllChannels />
+//           <DirectMessages />
+//         </SidebarMain>
+//       </Sidebar>
+//       <Body>
+//         <BodyHeader>
+//           <Title />
+//         </BodyHeader>
+//         <BodyMain>
+//           <ViewThread>
+//             <Body>
+//               <Message />
+//             </Body>
+//             <Composer />
+//           </ViewThread>
+//         </BodyMain>
+//       </Body>
+//     </>
+//   )
+// }
