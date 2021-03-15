bffa2eb7e566fcbcc15819560dab37cb007d7007
Bryce McLachlan <brycemclachlan@gmail.com>
2021-03-15 21:23:40 +0000

    added custom claims for hasura

--- services/auth/service/functions/index.js
+++ services/auth/service/functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // const functions = require('firebase-functions')
 
 // // Create and Deploy Your First Cloud Functions
@@ -7,3 +8,48 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// /https://hasura.io/blog/authentication-and-authorization-using-hasura-and-firebase/
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// On sign up.
+exports.processSignUp = functions.auth.user().onCreate((user) => {
+  // console.log(user);
+  // Check if user meets role criteria:
+  // Your custom logic here: to decide what roles and
+  // other `x-hasura-*` should the user get
+  // if (user.email && user.email.indexOf('@hasura.io') !== -1) {
+  //   customClaims = {
+  //     'https://hasura.io/jwt/claims': {
+  //       'x-hasura-default-role': 'admin',
+  //       'x-hasura-allowed-roles': ['user', 'admin'],
+  //       'x-hasura-user-id': user.uid
+  //     }
+  //   };
+  // }
+  // else {
+  const customClaims = {
+    'https://hasura.io/jwt/claims': {
+      'x-hasura-default-role': 'user',
+      'x-hasura-allowed-roles': ['user'],
+      'x-hasura-user-id': user.uid,
+    },
+  };
+  // }
+  // Set custom user claims on this newly created user.
+  return admin
+    .auth()
+    .setCustomUserClaims(user.uid, customClaims)
+    .then(() => {
+      // Update real-time database to notify client to force refresh.
+      // const metadataRef = admin.database().ref("metadata/" + user.uid);
+      // Set the refresh time to the current UTC timestamp.
+      // This will be captured on the client to force a token refresh.
+      // return metadataRef.set({refreshTime: new Date().getTime()});
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
