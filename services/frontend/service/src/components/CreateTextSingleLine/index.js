8f12ffb650f9e01aed07faec6ee9c7d64f78feaf
Bryce McLachlan <brycemclachlan@gmail.com>
2021-03-17 19:44:38 -0700

    added the abilty to create new conversations

---
+++ services/frontend/service/src/components/CreateTextSingleLine/index.js
@@ -0,0 +1,118 @@
+import { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import IconButton from '@material-ui/core/IconButton'
+import OutlinedInput from '@material-ui/core/OutlinedInput'
+import InputLabel from '@material-ui/core/InputLabel'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import FormControl from '@material-ui/core/FormControl'
+import Check from '@material-ui/icons/Check'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { gql, useMutation } from '@apollo/client'
+const MAKE_CONVERSATION = gql`
+  mutation(
+    $conversationName: String!
+    $conversationPublic: Boolean!
+    $channelId: Int!
+    $userId: Int!
+  ) {
+    insert_users_conversations_one(
+      object: {
+        conversation: {
+          data: {
+            name: $conversationName
+            public: $conversationPublic
+            channel_id: $channelId
+          }
+        }
+        user_id: $userId
+      }
+    ) {
+      conversation {
+        id
+        name
+        public
+        channel_id
+      }
+    }
+  }
+`
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%'
+  }
+}))
+
+const CreateTextSingleLine = ({
+  type = 'conversation',
+  currentUser,
+  currentState,
+  placeholder,
+  conversationPublic = true
+}) => {
+  const classes = useStyles()
+  const [text, setText] = useState('')
+  const [submitting, setSubmitting] = useState(false)
+  const [makeConversation] = useMutation(MAKE_CONVERSATION)
+  // used for testing without using apollo
+  // const makeConversation = async () => {
+  //   await new Promise((resolve) => {
+  //     setTimeout(resolve, 1000);
+  //   });
+  // };
+
+  const submit = async (text) => {
+    setSubmitting(true)
+    if (type === 'conversation') {
+      // await makeConversation();
+      await makeConversation({
+        variables: {
+          conversationName: text,
+          conversationPublic: conversationPublic,
+          channelId: currentState.channel,
+          userId: currentUser.id
+        }
+      })
+    }
+    setSubmitting(false)
+    setText('')
+  }
+  return (
+    <FormControl variant='outlined' className={classes.root}>
+      <InputLabel>{placeholder}</InputLabel>
+      <OutlinedInput
+        disabled={submitting}
+        type='text'
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        onKeyUp={(e) => {
+          if (e.which === 13) {
+            submit(text)
+          }
+        }}
+        endAdornment={
+          !submitting
+            ? (
+              <InputAdornment position='end'>
+                <IconButton
+                  onClick={() => {
+                    submit(text)
+                  }}
+                  edge='end'
+                >
+                  <Check />
+                </IconButton>
+              </InputAdornment>
+              )
+            : (
+              <InputAdornment position='end'>
+                <CircularProgress />
+              </InputAdornment>
+              )
+        }
+        labelWidth={placeholder.length * 9}
+      />
+    </FormControl>
+  )
+}
+export default CreateTextSingleLine
