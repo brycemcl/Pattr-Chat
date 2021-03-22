c51394830335200e2ad4614b30b10bdb3d37927b
Bryce McLachlan <brycemclachlan@gmail.com>
2021-03-22 22:35:19 +0000

    fixed bug where channel did not automatically switch over

--- services/frontend/service/src/components/ContextSwitcher/DataWrapper.js
+++ services/frontend/service/src/components/ContextSwitcher/DataWrapper.js
@@ -1,7 +1,7 @@
 import Selector from './Selector'
 import Spinner from '../Spinner'
 import useChannels from './useChannels'
-
+import { useState, useEffect } from 'react'
 const DataWrapper = ({
   currentUser,
   currentState,
@@ -16,10 +16,34 @@ const DataWrapper = ({
     error,
     loading
   } = useChannels({ currentUser, currentState, setCurrentState })
+
+  const [value, setValue] = useState(false)
+  const currentChannel = channels.filter((channel) => {
+    return channel.id === currentState.channel
+  })?.[0]
+  const toggleChannelSelectorOpen = () => {
+    if (currentChannel) {
+      setValue((cs) => !cs)
+    } else {
+      setValue(true)
+    }
+  }
+  useEffect(() => {
+    if (!currentChannel) {
+      setValue(true)
+    } else {
+      setValue(false)
+    }
+  }, [currentChannel])
+
   if (loading) return <Spinner />
   if (error) return <p>Error :(</p>
+
   return (
     <Selector
+      value={value}
+      currentChannel={currentChannel}
+      toggleChannelSelectorOpen={toggleChannelSelectorOpen}
       channels={channels}
       publicConversations={publicConversations}
       privateConversations={privateConversations}
