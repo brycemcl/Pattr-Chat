58fb6bfb342f2fd69cbf84f33d47551e9e9ec29e
Bryce McLachlan <brycemclachlan@gmail.com>
2021-03-22 10:27:08 -0700

    changed some styling

--- services/frontend/service/src/components/ContextSwitcher/Selector.js
+++ services/frontend/service/src/components/ContextSwitcher/Selector.js
@@ -1,10 +1,18 @@
 import SelectorChannel from './SelectorChannel'
 import SelectorConversation from './SelectorConversation'
 import { useState } from 'react'
-import SwapHorizIcon from '@material-ui/icons/SwapHoriz'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import AppBar from '@material-ui/core/AppBar'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  channelSelector: {
+    height: '80px',
+    fontSize: '18px'
+  }
+}))
 
 const Selector = ({
   channels,
@@ -15,6 +23,7 @@ const Selector = ({
   currentState,
   currentUser
 }) => {
+  const classes = useStyles()
   const [value, setValue] = useState(false)
   const currentChannel = channels.filter((channel) => {
     return channel.id === currentState.channel
@@ -26,6 +35,7 @@ const Selector = ({
     <div>
       <AppBar position='static' color='default'>
         <Tabs
+          className={classes.channelSelector}
           value={0}
           onChange={() => toggleChannelSelectorOpen()}
           indicatorColor='primary'
@@ -33,11 +43,12 @@ const Selector = ({
           variant='fullWidth'
         >
           <Tab
+            className={classes.channelSelector}
             label={
               <div>
                 {value ? 'Select a Channel' : `${currentChannel?.name} Channel`}
                 &nbsp;
-                <SwapHorizIcon fontSize='inherit' />
+                <ExpandMoreIcon fontSize='inherit' />
               </div>
             }
           />
